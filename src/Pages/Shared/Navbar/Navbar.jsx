ec2d941911aae0c6eaf143059bd37e9240d95b1d
Sultana Razia <sultanarazia483@gmail.com>
2024-11-18 11:27:11 +0600

    Style Header and create simple banner

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -4,8 +4,14 @@ import logo from '../../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const navItems = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+    </>
+
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 h-28 mb-5">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -25,15 +31,7 @@ const Navbar = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navItems}
                     </ul>
                 </div>
                 <Link to='/' className="btn btn-ghost text-xl">
@@ -42,17 +40,7 @@ const Navbar = () => {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {navItems}
                 </ul>
             </div>
             <div className="navbar-end">
