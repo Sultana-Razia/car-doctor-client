c9f888d331b044658d29497d06d50d2f26682616
Sultana Razia <sultanarazia483@gmail.com>
2024-11-20 21:37:19 +0600

    Sign Up page setup and create Auth Context setup

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FaFacebookF } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import img from '../../assets/images/login/login.svg';
-import { Link } from 'react-router-dom';
 
 
-const Login = () => {
+const SignUp = () => {
 
-    const handleLogin = event => {
+    const handleSignUp = event => {
         event.preventDefault();
-        console.log("login")
     }
 
     return (
@@ -20,35 +19,41 @@ const Login = () => {
                     <img src={img} alt="image" />
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <form onSubmit={handleLogin} className="card-body">
-                        <h1 className="text-[#444444] text-center text-4xl mb-8 font-semibold">Login</h1>
+                    <form onSubmit={handleSignUp} className="card-body">
+                        <h1 className="text-[#444444] text-center text-4xl mb-8 font-semibold">Sign Up</h1>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="Your Name" className="input input-bordered" required />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                            <input type="email" name="email" placeholder="Your Email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <input type="password" name="password" placeholder="Confirm Password" className="input input-bordered" required />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn bg-[#FF3811] text-white text-xl font-semibold">Sign In</button>
+                            <button className="btn bg-[#FF3811] text-white text-xl font-semibold">Sign Up</button>
                         </div>
                     </form>
                     <div className='text-center'>
-                        <p>Or Sign in with</p>
+                        <p>Or Sign up with</p>
                         <div className='flex gap-3 justify-center mt-8'>
                             <FaFacebookF />
                             <FaLinkedinIn />
                             <FcGoogle />
                         </div>
-                        <p className='text-[#737373] text-lg mb-8 mt-8'>New to cars doctor <Link to='/signUp' className='text-[#FF3811] font-semibold'>Sign Ip</Link></p>
+                        <p className='text-[#737373] text-lg mb-8 mt-8'>Already have an account? <Link to='/login' className='text-[#FF3811] font-semibold'>Login</Link></p>
                     </div>
                 </div>
             </div>
@@ -56,4 +61,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
